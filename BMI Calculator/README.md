a4293b7d68f3233cdc76d2d00106e23df4109a2a
Sanskruti Santosh Kute <sanskruti.kute.5083723@ves.ac.in>
2026-06-04 17:52:54 +0530

    Add files via upload

---
+++ BMI Calculator/README.md
@@ -0,0 +1,19 @@
+# BMI Calculator
+
+This is a simple BMI Calculator developed using Python.
+
+## Features
+- Takes weight as input
+- Takes height as input
+- Calculates BMI
+- Displays BMI category:
+  - Underweight
+  - Normal Weight
+  - Overweight
+  - Obese
+
+## Technology Used
+- Python
+
+## Author
+Sanskruti Santosh Kute
---
+++ BMI Calculator/bmi_calculator.py
@@ -0,0 +1,18 @@
+print("===== BMI CALCULATOR =====")
+
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in meters: "))
+
+bmi = weight / (height * height)
+
+print("\n===== RESULT =====")
+print("Your BMI is:", round(bmi, 2))
+
+if bmi < 18.5:
+    print("Category: Underweight")
+elif bmi < 25:
+    print("Category: Normal Weight")
+elif bmi < 30:
+    print("Category: Overweight")
+else:
+    print("Category: Obese")
